eb31bd885c0fd598d0efd985939869dbb23c4400
dguti088 <142689617+dguti088@users.noreply.github.com>
2023-09-08 20:07:52 -0400

    Update Lab1MemoryGame.py

--- Lab1MemoryGame.py
+++ Lab1MemoryGame.py
@@ -11,6 +11,12 @@ print ("Hello, Welcome to Dani's Memory Game! :)")
 #class will provide logic for game using imported classes 
 
 class MemoryGame 
-def __init__(self): 
+def __init__(self):  
+  self._display = LCDDisplay(sda = 0, scl = 1, i2cd = 0 )
+  self._buzzer = PassiveBuzzer (16) 
+  self-_
+  self._buttonreset = Button(17, "reset", buttonhandler = self) 
+  self._start() #start of game 
+  
 
 
