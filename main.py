863fa94d61e6a67a7c418ec3e8dbf462dec88e28
dguti088 <142689617+dguti088@users.noreply.github.com>
2023-09-21 21:48:21 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
 import time
-import machine
-import utime 
-from Lights import * 
-from Buzzer import *
-from Displays import * 
 time.sleep(0.1) # Wait for USB to become ready
+
+from RoomController import *
 
 print("Hello, Pi Pico!")
 
-#created instance of myLED light 
-myled= Light(25, "Internal LED")
+myroom = RoomController()
 
-#turn on LED 
-myled.on()
+myroom.run()
 
-#wait for a few seconds 
-utime.sleep(5)
 
-#Turn off the LED 
-myled.off()
 
-extled = DimLight(22, "Pink Led")
-extled.on()
-utime.sleep(1)
-extled.setBrightness(100)
-utime.sleep(1)
-extled.off()
-extled.upDown()
-
-mybuzzer = PassiveBuzzer(16)
-mybuzzer.beep()
-
-mydisplay = LCDDisplay(sda =0, scl =1, i2cid =0)
-mydisplay.showText('Python is fun!')
